07d59aa2178ce391748bb00f9673054db8bf2b86
jillianhunter1 <112662227+jillianhunter1@users.noreply.github.com>
2025-02-20 16:35:30 -0800

    melody.ohm complete

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as ohm from "ohm-js";
 
-const grammar = ohm.grammar(fs.readFileSync("src/melody.ohm"));
+const grammar = ohm.grammar(fs.readFileSync("src/melody.ohm", "utf-8"));
 
 export default function parse(sourceCode) {
   const match = grammar.match(sourceCode);
